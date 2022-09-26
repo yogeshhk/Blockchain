4efdcf5977999b30fdf68f462482f6146b59e2be
yogeshhk <yogeshkulkarni@yahoo.com>
2022-09-26 18:32:34 +0530

    Started adding code

---
+++ LaTeX/src/blockchain/blockchain.py
@@ -0,0 +1,24 @@
+# Create Blockchain
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+
+# Part 1 - Building a Blockchain
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash
+                 }
+        self.chain.append(block)
+        return block
+
+# Part 2 - Mining the Blockchain
